37510c217f9deba805286c3707fe43301cb368ff
m-nakan70 <mio.nakanishi70@gmail.com>
2023-10-06 10:55:50 +0900

    gmail送信可(security対応）

--- src/main/java/com/example/stockapp/controller/SendMailController.java
+++ src/main/java/com/example/stockapp/controller/SendMailController.java
@@ -1,18 +1,14 @@
 package com.example.stockapp.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.example.stockapp.dao.MemoForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class SendMailController {

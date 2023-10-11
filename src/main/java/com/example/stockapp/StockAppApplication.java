ff8a2a3eba3e232257765fc458271c801af7b4ea
m-nakan70 <mio.nakanishi70@gmail.com>
2023-10-11 16:57:32 +0900

    賞味期限タイマー通知機能テスト。。。

--- src/main/java/com/example/stockapp/StockAppApplication.java
+++ src/main/java/com/example/stockapp/StockAppApplication.java
@@ -1,13 +1,26 @@
 package com.example.stockapp;
 
+import com.example.stockapp.controller.NotifyTimer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Date;
+import java.util.Timer;
 @SpringBootApplication
+@EnableScheduling
 public class StockAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockAppApplication.class, args);
+				// Timerクラスのオブジェクトを作成
+			Timer time = new Timer();
+
+			System.out.println("実行開始" + new Date());
+
+			// 一定間隔で処理を開始する
+			// SampleTaskを、3秒後に、5秒間隔で実行する
+			time.scheduleAtFixedRate(new NotifyTimer(), 3000, 5000);
+		}
 	}
 
-}
+
---
+++ src/main/java/com/example/stockapp/controller/NotifyTimer.java
@@ -0,0 +1,41 @@
+package com.example.stockapp.controller;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class NotifyTimer extends TimerTask {
+
+    static int i = 1;
+    public void run() {
+        try {
+
+            System.out.println("-----------------------------------------");
+            System.out.println(i + "回目タスク開始" + new Date());
+
+            //3秒間停止する
+            Thread.sleep(3000);
+
+            System.out.println(i + "回目タスク終了" + new Date());
+
+            i = i + 1;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+//    private int i = 0;
+//    private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+//
+//    @Scheduled(initialDelay=0, fixedDelay=3000)
+//    public void execute() {
+//        System.out.println("実行回数: " + ++i + ", 実行時間: " + sdf.format(new Date()));
+//
+//    }
+//}
+
